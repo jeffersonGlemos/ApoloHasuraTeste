6761ae93d11d9434fd5a8e831f29483f55e252a7
Jefferson Gonçalves <jefferson@datweb.com.br>
2020-08-24 15:08:00 -0400

    FirstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import gql from 'graphql-tag';
+import {Apollo} from 'apollo-angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import {NgxQrcodeElementTypes} from 'ngx-qrcode2';
+
+const SUBSCRIBE_TO_AGENDAMENTO = gql`
+ subscription MyQuery {
+  ferramenta_agendamento {
+    descricao
+    inicio
+    fim
+    status
+    uuid
+    uuid_localidade
+  }
+}
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DemonstrativoAgenda';
+  agendamento: any;
+  onlineIndicator: any;
+  loading = true;
+  events: any[];
+  options: any;
+
+  constructor(private http: HttpClient, private apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.options = {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      defaultDate: '2020-08-04',
+      header: {
+        left: 'prev,next',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      }
+    };
+    this.apollo.subscribe({
+      query: SUBSCRIBE_TO_AGENDAMENTO,
+    }).subscribe((data) => {
+      // @ts-ignore
+      // tslint:disable-next-line:only-arrow-functions typedef
+      console.log('got data ', data.data.agendamento);
+      // this.events = data.data;
+      // @ts-ignore
+      // tslint:disable-next-line:only-arrow-functions typedef
+      this.agendamento = Array.from(data.data.agendamento).map(function({descricao, fim, inicio, status}) {
+        return {
+          title: descricao,
+          start: inicio,
+          end: fim,
+          color  : status
+        };
+      });
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FullCalendarModule} from 'primeng/fullcalendar';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {GraphQLModule} from './graphql.module';
+import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular-link-http';
+import {WebSocketLink} from 'apollo-link-ws';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {environment} from '../environments/environment';
+import {QRCodeModule} from 'angularx-qrcode';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    GraphQLModule,
+    QRCodeModule,
+    FullCalendarModule
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      const token = environment.token;
+
+      return {
+        cache: new InMemoryCache(),
+        link: new WebSocketLink({
+          options: {
+            reconnect: true,
+            connectionParams: {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            }
+          },
+          uri: 'ws://pgadmin.datweb.com.br:8080/v1/graphql'
+        })
+      };
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
